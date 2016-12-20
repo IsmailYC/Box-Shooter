4ab078418a93fa28e07551e1fcf14e1caffda775
YOUSSOUF ISMAIL CHERIFI <youcef.ismail.cherifi@gmail.com>
2016-12-20 20:05:00 +0100

    Add files via upload

---
+++ Scripts/SpawnGameObjects.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGameObjects : MonoBehaviour
+{
+	// public variables
+	public float secondsBetweenSpawning = 0.1f;
+	public float xMinRange = -25.0f;
+	public float xMaxRange = 25.0f;
+	public float yMinRange = -25.0f;
+	public float yMaxRange = 25.0f;
+	public float zMinRange = -25.0f;
+	public float zMaxRange = 25.0f;
+	public GameObject[] spawnObjects; // what prefabs to spawn
+
+	public void Spawn ()
+	{
+		Vector3 spawnPosition;
+
+		// get a random position between the specified ranges
+		spawnPosition.x = Random.Range (xMinRange, xMaxRange);
+		spawnPosition.y = Random.Range (yMinRange, yMaxRange);
+		spawnPosition.z = Random.Range (zMinRange, zMaxRange);
+
+		// determine which object to spawn
+		int objectToSpawn = Random.Range (0, spawnObjects.Length);
+
+		// actually spawn the game object
+		GameObject spawnedObject = Instantiate (spawnObjects [objectToSpawn], spawnPosition, transform.rotation) as GameObject;
+
+		// make the parent the spawner so hierarchy doesn't get super messy
+		spawnedObject.transform.parent = gameObject.transform;
+        if (GameManager.gm.gameState == GameManager.GameState.play)
+            Invoke("Spawn", secondsBetweenSpawning);
+	}
+}
